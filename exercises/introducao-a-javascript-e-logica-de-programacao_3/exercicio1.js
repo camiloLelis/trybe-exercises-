5cb274bd5cf168f5c074a0b1a9b1a760573abfb4
camiloLelis <lafirmafuerte@gmail.com>
2021-02-28 12:19:19 -0300

    exercicio1 do bloco4.3 concluído

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,13 @@
+let n = 5;
+let imprima = "";
+for (let index = 0; index < n; index += 1) {
+
+    for (let i = 0; i < n; i++) {
+        imprima = imprima + "*";
+
+    }
+    console.log(imprima);
+    imprima= "";
+}
+
+
