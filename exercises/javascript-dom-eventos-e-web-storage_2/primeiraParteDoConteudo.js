dd0f77691b1b9a88743274a0283d86c6d78fde90
camiloLelis <lafirmafuerte@gmail.com>
2021-03-04 20:55:08 -0300

    exercicios do bloco 5.2, primeira parte do conteudo(antes da aula ao vivo- feitos)

--- exercises/javascript-dom-eventos-e-web-storage_2/primeiraParteDoConteudo.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/primeiraParteDoConteudo.js
@@ -1,3 +1,34 @@
-let elementoOndeVoceEsta = document.querySelector(#elementoOndeVoceEsta).parentNode;
+let elementoOndeVoceEsta = document.querySelector("#elementoOndeVoceEsta");
+    console.log (elementoOndeVoceEsta);
 
-console.log (elementoOndeVoceEsta);
+// segundo parte
+
+console.log(elementoOndeVoceEsta.parentNode);
+
+// terceira parte 
+
+document.querySelector('#primeiroFilhoDoFilho').innerHTML='filho meuuuu..!!!';
+
+//quarta parte
+
+console.log(document.querySelector('#pai').firstElementChild);
+
+// quinta parte
+
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+
+// sexta parte
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+// setima parte
+
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
+
+// oitava parte
+
+
+
+console.log(document.querySelector('#pai').childNodes[5]);
+
