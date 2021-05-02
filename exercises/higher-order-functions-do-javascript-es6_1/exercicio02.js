8a7fc6436454a6a1b37086c4a9a2a99f6c7d48c4
camiloLelis <lafirmafuerte@gmail.com>
2021-05-02 18:30:44 -0300

    exercicio-2-ajustes

--- exercises/higher-order-functions-do-javascript-es6_1/exercicio02.js
+++ exercises/higher-order-functions-do-javascript-es6_1/exercicio02.js
@@ -4,13 +4,13 @@ e uma função que checa se o número apostado é igual ao número sorteado.
 O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). */
 
 function checkDraw(bet, drawn) {
-    return bet === drawn;
+  return bet === drawn;
 }
 
 const prizeDraw = (numberBet, callback) => {
-    const generate = Math.floor((Math.random()*5)+1);
-    const check = callback(numberBet, generate);
-    //console.log(generate);
-    return (check === true) ? "Parabéns você ganhou": "Tente novamente";
+  const generate = Math.floor((Math.random()*5)+1);
+  const check = callback(numberBet, generate);
+  // console.log(generate);
+  return ( check === true ) ? "Parabéns você ganhou" : "Tente novamente";
 }
-console.log(prizeDraw(2, checkDraw));
+//console.log(prizeDraw(2, checkDraw));
