c4462b6f3ee2b48addd88416ad1b32003dfd705c
camiloLelis <lafirmafuerte@gmail.com>
2021-05-07 13:03:19 -0300

    exercise-6

--- exercises/higher-order-functions-do-javascript-es6_4/exercise6.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise6.js
@@ -5,8 +5,11 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
   // escreva seu código aqui
-  const nome = students.map((student,index) => ({ name: student, average: grades[index].reduce((acc,curr) => acc + curr) / grades[index].length}));
-  return nome;
+  return students
+    .map((student,index) => ({ 
+      name: student, 
+      average: grades[index].reduce((acc,curr) => acc + curr) / grades[index].length
+    }));
 }
 
 const expected = [
