3bdfbd56461b365ce77ac51a56d0977e02167b01
camiloLelis <lafirmafuerte@gmail.com>
2021-05-12 16:20:52 -0300

    exercise-1

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
+++
@@ -1,8 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorNum =numbers[0];
-for (let index = 0; index < numbers.length; index++) {
-    if (numbers[index] < menorNum) {
-        menorNum = numbers[index];
-    }
-}
-console.log(menorNum);
