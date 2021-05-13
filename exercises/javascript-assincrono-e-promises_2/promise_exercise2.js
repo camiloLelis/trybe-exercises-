0f7f16d4186704136d750478f673e65d587bc2c1
camiloLelis <lafirmafuerte@gmail.com>
2021-05-13 16:16:21 -0300

    exercise-2

---
+++ exercises/javascript-assincrono-e-promises_2/promise_exercise2.js
@@ -0,0 +1,14 @@
+const PromiseExercise = () => {
+  testPromise = new Promise ((resolve,reject) => {
+   const arrayMath = Array.from(
+        {length:10},
+        () => Math.floor(Math.random() * 50) +1,
+    );
+
+    const soma = arrayMath.map(number => number * number)
+      .reduce((acc, number) => acc + number); 
+    soma < 8000 ? resolve(soma) : reject(soma);
+  });
+  testPromise.then((soma) => console.log(`resolvido codigo :${soma}`)).catch((soma) => console.log(`rejeitada codigo: ${soma}` ));
+}
+PromiseExercise();
