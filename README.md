67ebc986e55248e6c2bf102fb40afbd5e9f7e02d
camiloLelis <lafirmafuerte@gmail.com>
2021-05-18 20:17:17 -0300

    exercicios-b10-dia1-jest

--- README.md
+++
@@ -1,5 +0,0 @@
-CURSO DA TRYBE
-
-COMEÇANDO O REPOSITÓRIO DATA : 08/02/2021
-
-hoje estou conseguindo criar e inserir as chaves ssh 
--- exerc_conteu_part1/aula bloco 4.2/array3.js
+++
@@ -1,4 +0,0 @@
-//indexof = para descobri onde este um valor, qual é seu indice (chave)
-let tasksList=['Tomar café','Reunião','Brincar com o cachorro'];
-let indexOfTask= tasksList.indexOf('Reunião');
-console.log(indexOfTask);
