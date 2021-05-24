db4c79e7f7515e47bbc96d2fe10808d49a73ff24
camiloLelis <lafirmafuerte@gmail.com>
2021-05-24 15:58:24 -0300

    exercise-2

---
+++ exercises/testes-automatizados-com-jest_2/exercise2.test.js
@@ -0,0 +1,28 @@
+const users = {
+    4: { name: 'Mark' },
+    5: { name: 'Paul' }
+    };
+    
+    const findUserById = (id) => {
+      return new Promise((resolve, reject) => {
+          if (users[id]) {
+            return resolve(users[id]);
+          };
+    
+          return reject({ error: 'User with ' + id + ' not found.' });
+      });
+    };
+    
+    const getUserName = (userID) => {
+      return findUserById(userID).then(user => user.name);
+    };
+describe('envolvendo resolve e reject em uma descrição', () => {
+    it("resolve getUserName", () => {
+         expect.assertions(1);
+         return getUserName(4).then(data => expect(data).toEqual('Mark'));
+      });
+    it("reject getUserName", () =>{
+        expect.assertions(1);
+        return getUserName(2).catch(error => expect(error).toEqual({error: 'User with 2 not found.'}));
+    });
+})
